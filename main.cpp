68eb92334c4e6ddfbce2ed9d3986de52d81746f5
Axinte Codrin <6564791+codrin-axinte@users.noreply.github.com>
2017-11-16 17:37:40 +0000

    Fixed the issue for "ssa"

--- main.cpp
+++ main.cpp
@@ -5,114 +5,87 @@
 #include <sstream>
 #include <vector>
 
-using namespace std; 
+using namespace std;
 
 vector<string> split(string value, char token);
 string GetAnagram(unsigned maxLength);
 void FindSolution(string anagram, string word);
-bool ContainsAll(string anagram, string word);
 
-unsigned bestWeight = 0;
 string solution;
 
-int main(){
-  // Get the anagram from the input and validate it
-  string anagram = GetAnagram(6);
-  
-  ifstream infile("db.csv");
-  string line;
-  
-  while(infile >> line){
-    string word = split(line, ',').at(0);
-    if(word.length() > anagram.length()) continue; 
-    FindSolution(anagram, word); 
-  }
-  if(solution == ""){
-  	cout << "No solution was found!" << endl;
-  	
-  } else {
-  	cout << "Best solution: " << solution << endl;
-  }
-  
-  return 0;
+int main() {
+	string anagram = GetAnagram(6);
+	cout << "Please wait! Cracking..." << endl;
+	ifstream infile("db.csv");
+	string line;	
+	while (infile >> line) {		
+		string word = split(line, ',').at(0);
+		if (word.length() > anagram.length()) continue;
+		FindSolution(anagram, word);		
+	}
+	if (solution == "") 
+		std::cout << "No solution was found!" << endl;	
+	else 
+		std::cout << "Best solution: " << solution << endl;
+		
+	system("pause");
+
+	return 0;
 }
-
-// zza -> a, OZR -> or, tca -> act
-/* Tinker
-	
-  
-*/  
-
 
 
 void FindSolution(string anagram, string word) {
 	bool valid = true;
-	map<unsigned, bool> slots;
-	cout << "Word: " << word << endl;
-	cout << "=================" << endl;
-    for(unsigned i=0; i < word.length(); i++){ // 0 -> a
-	  bool found = false;
-      
-      for(unsigned j=0; j < anagram.length(); j++){
-      	cout << word[i] << " -> " << anagram[j] << " ";
-  	 	
-  	 	if(slots[j] == true) { // tca -> 0,1,2 -> 
-	 		cout << "@occupied";
-	 	} else if(anagram[j] == word[i]) {
-           slots[j] = true;
-           found = true;
-           cout << "@found";
-    	} else {
-    		slots[j] = false;
-    	}
-    	cout << endl;
-      }
-      
-      cout << "----------" << endl;
-      
-      if(found == false) {
-      	valid = false;
-      	break;
-      }
-    }
-    
-	if(valid) {
-		if(solution.length() > word.length()) return;
-		solution = word;
-		cout << "Is Valid" << endl;
+	map<unsigned, bool> slots;	
+	for (unsigned i = 0; i < word.length(); i++) {
+		bool found = false;
+		for (unsigned j = 0; j < anagram.length(); j++) {			
+			if (anagram[j] == word[i] && !found) {
+				if (!slots[j]) {
+					found = true;
+					slots[j] = true;										
+				}	
+			}		
+		}
+		if (found == false) {
+			valid = false;
+			break;
+		}
 	}
-	
-	cout << "=================" << endl;
+
+	if (valid && solution.length() < word.length()) {
+		solution = word;		
+	}	
 }
 
 
 
 string GetAnagram(unsigned maxLength) {
-  string a;
-  do {
-  cout << "Anagram: ";
-  cin >> a;
-  cout << endl;
-  if(a.length() > maxLength){
-    cout << "Anagram can have " << maxLength << " maximum letters" << endl;
-  }
-  } while(a.length() > maxLength);
-  
-  return a;
+	string a;
+	do {
+		std::cout << "Anagram: ";
+		cin >> a;
+		std::cout << endl;
+		if (a.length() > maxLength) {
+			std::cout << "Anagram can have " << maxLength << " maximum letters" << endl;
+		}
+	} while (a.length() > maxLength);
+
+	return a;
 }
 
 
 
-vector<string> split(string value, char token){
-    stringstream ss(value);
-    vector<string> vect;
-    
-    while(ss.good()){
-      string substr;
-      getline(ss, substr, ',');
-      vect.push_back(substr);
-    }
-    
-    return vect;
+vector<string> split(string value, char token) {
+	stringstream ss(value);
+	vector<string> vect;
+
+	while (ss.good()) {
+		string substr;
+		getline(ss, substr, ',');
+		vect.push_back(substr);
+	}
+
+	return vect;
 }
 
